aa99aa7f9e02c0cb63fceea573a1caacf8d86084
estein1988 <estein1988@gmail.com>
2020-08-26 16:39:53 -0600

    Complete boiler-plate setup for M:N database

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,18 @@
 class FlightsController < ApplicationController
+
+    def index
+        @flights = Flight.all 
+        render json: @flights, include: [:passenger, :airplane]
+        #note singular above
+        #include - takes a relationship
+    end
+
+    def create
+        @flight = Flight.create(
+            airplane_id: params[:airplane_id],
+            passenger_id: params[:passenger_id] 
+        )
+        render json: @flgiht, incldude: [:passenger, :airplane]
+    end
+
 end
--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,22 @@
 class PassengersController < ApplicationController
+
+    def index 
+        @passengers = Passenger.all 
+        render json: @passengers, include: [:flights, :airplanes]
+        #note plural form in include
+        #this "include syntax is really the biggest different with a M:N"
+    end
+
+    def show
+        @passenger = Passenger.find(params[:id])
+        render json: @passengers, include: [:flights, :airplanes]
+        #find by - finds by key.value pair
+        #find is by id
+    end
+
+    def create
+        @passenger = Passenger.create(name: params[:name])
+        render json: @passenger
+    end 
+
 end
