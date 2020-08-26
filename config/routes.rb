dde0adfc8d0ecf40401a0478cbb68423b4aab483
estein1988 <estein1988@gmail.com>
2020-08-26 15:52:29 -0600

    Adds resources and models.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :flights
+  resources :passengers
+  resources :airplanes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
